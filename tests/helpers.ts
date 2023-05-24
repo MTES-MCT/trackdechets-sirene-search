9a7ba42ddcbd1b3d5a7f5fe39f07243208235c9f
elias@showk.me <elias@showk.me>
2023-05-24 09:50:58 +0200

    fix elasticsearch in CI

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,11 +1,9 @@
 import { elasticSearchClient } from "../src/common/elastic";
 import { INDEX_NAME_INSEE_PREFIX } from "../src/indexation/indexInsee.helpers";
 
-
 export function refreshElasticSearch() {
   return elasticSearchClient.indices.refresh();
 }
-
 
 export async function resetDatabase() {
   const indices = await elasticSearchClient.cat.indices({
@@ -13,8 +11,9 @@ export async function resetDatabase() {
     format: "json"
   });
 
-  const indicesNames: string[] = indices.body
-    .map((info: { index: string }) => info.index);
+  const indicesNames: string[] = indices.body.map(
+    (info: { index: string }) => info.index
+  );
   if (indicesNames.length) {
     await elasticSearchClient.indices.delete({ index: indicesNames.join(",") });
   }
