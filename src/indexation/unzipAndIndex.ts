65c9d5c37dee1244cdaf51f53bd54e7e38d280d9
elias@showk.me <elias@showk.me>
2024-04-02 11:33:45 +0200

    fix unzip insee file

--- src/indexation/unzipAndIndex.ts
+++ src/indexation/unzipAndIndex.ts
@@ -1,10 +1,30 @@
+import fs from "fs/promises";
+import path from "path";
 import StreamZip from "node-stream-zip";
 import { IndexProcessConfig } from "./types";
 import {
   createIndexRelease,
   streamReadAndIndex
 } from "./elasticSearch.helpers";
+import { logger } from "..";
 import { getCsvPath } from "./indexInsee.helpers";
+
+/**
+ * CSV filename can change with time
+ */
+async function renameFile(directoryPath: string, newFilePath: string) {
+  const regex = /Stock[\w]+_utf8\.csv/;
+  const files = await fs.readdir(directoryPath);
+  const fileToRename = files.find(file => regex.test(file));
+
+  if (fileToRename) {
+    const oldFilePath = path.join(directoryPath, fileToRename);
+    await fs.rename(oldFilePath, newFilePath);
+    logger.info(`File renamed from ${fileToRename} to ${newFilePath}`);
+  } else {
+    logger.info("No matching file found");
+  }
+}
 
 /**
  * Streaming unzip, formatting documents and index them
@@ -17,7 +37,9 @@ export const unzipAndIndex = async (
   const indexName = await createIndexRelease(indexConfig);
   const zip = new StreamZip.async({ file: zipPath });
   const csvPath = getCsvPath(destination, indexConfig);
-  await zip.extract(indexConfig.csvFileName, csvPath);
+  // extract everything from the zip
+  await zip.extract(null, destination);
+  await renameFile(destination, csvPath);
   await zip.close();
   await streamReadAndIndex(csvPath, indexName, indexConfig);
   return indexName;
